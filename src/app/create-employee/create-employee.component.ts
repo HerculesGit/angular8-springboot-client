45e3ce38b65a0026e7aa366116c85f3407d3b238
HerculesGit <hercules.silva@dce.ufpb.br>
2019-11-04 00:33:31 -0300

    Part 4

--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
+import { Employee } from '../models/employee';
 
 @Component({
   selector: 'app-create-employee',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEmployeeComponent implements OnInit {
 
-  constructor() { }
+  employee: Employee = new Employee();
+  submitted = false;
+
+  constructor(private employeeService: EmployeeService,
+    private router: Router) { }
 
   ngOnInit() {
   }
 
+  newEmployee(): void {
+    this.submitted = false;
+    this.employeeService
+  }
+
+  save() {
+    this.employeeService.createEmployee(this.employee)
+      .subscribe(data => console.log(data), error => console.log(error));
+
+    this.employee = new Employee();
+    this.goToList();
+
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();
+  }
+
+  goToList() {
+    this.router.navigate(['/employees']);
+  }
 }
